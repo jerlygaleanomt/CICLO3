79e580e24699554d011e977e082cbefcf17ecbb1
Jerly Galeano <jerly.galeano.mt@usa.edu.co>
2022-09-13 19:04:01 -0500

    primer commit del proyecto

---
+++ src/main/java/com/ciclo3/proyectog24/Ejercicio1.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ciclo3.proyectog24;
+
+/**
+ *
+ * @author jerly
+ */
+public class Ejercicio1 {
+    
+}
